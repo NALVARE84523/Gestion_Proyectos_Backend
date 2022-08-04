d583eff71d645f4883e83cb74b446f0e1a4455d0
nalvare84523 <nalvare84523@universidadean.edu.co>
2022-08-03 23:22:48 -0500

    Proyecto finalizado

---
+++ helpers/email.js
@@ -0,0 +1,55 @@
+import nodemailer from "nodemailer";
+
+export const emailRegistro = async (datos) => {
+  const { email, nombre, token } = datos;
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  // Informacion del email
+  const info = await transport.sendMail({
+    from: '"UpTask - Administrador de Proyectos <cuentas@uptask.com>"',
+    to: email,
+    subject: "UpTask - Comprueba tu cuenta",
+    text: "Comprueba tu cuenta en UpTask",
+    html: ` <p>Hola: ${nombre}, comprueba tu cuenta en UpTask</p>
+    <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace: </p>
+    <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar cuenta</a>
+
+    <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
+
+    `,
+  });
+};
+
+export const emailOlvidePassword = async (datos) => {
+  const { email, nombre, token } = datos;
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  // Informacion del email
+  const info = await transport.sendMail({
+    from: '"UpTask - Administrador de Proyectos <cuentas@uptask.com>"',
+    to: email,
+    subject: "UpTask - Restablece tu password",
+    text: "Restablece tu password",
+    html: ` <p>Hola: ${nombre}, has solicitado restablecer tu password</p>
+    <p>Sigue el siguiente enlace para generar un nuevo password: </p>
+    <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Restablecer password</a>
+
+    <p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>
+
+    `,
+  });
+};
